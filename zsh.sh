bfa1bc3a0c072833b867cab6a32f969cd972525d
aiktb <aiktb@outlook.com>
2023-04-29 13:25:17 +0800

    Add zsh-search

--- zsh.sh
+++ zsh.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# Or yum.
 sudo apt install zsh -y
 # Install oh-my-zsh.
 0>/dev/null sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 export ZSH_CUSTOM
+# Configure plugins.
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM}"/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git "${ZSH_CUSTOM}"/plugins/zsh-autosuggestions
-sed -i 's/^plugins=.*/plugins=(git extract sudo thefuck autojump jsontools colored-man-pages zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
-
+git clone https://github.com/zsh-users/zsh-history-substring-search "${ZSH_CUSTOM}"/plugins/zsh-history-substring-search
+sed -i 's/^plugins=.*/plugins=(git\n extract\n sudo\n autojump\n jsontools\n colored-man-pages\n zsh-autosuggestions\n zsh-syntax-highlighting\n zsh-history-substring-search\n)/g' ~/.zshrc
 # Install powerlevel10k and configure it.
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM}"/themes/powerlevel10k
 sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
