c74e66c74ce143540fceb9ead723f3832428c3ec
aiktb <aiktb@outlook.com>
2023-05-25 21:55:15 +0800

    Refactor: 2 to 1

--- shel.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo cp ~/.zshrc /etc/skel/
-sudo cp ~/.p10k.zsh /etc/skel/
-sudo cp -r ~/.oh-my-zsh /etc/skel/
-sudo chmod -R 755 /etc/skel/
-sudo chown -R root:root /etc/skel/
--- zsh.sh
+++ zsh.sh
@@ -14,6 +14,11 @@ sed -i 's/^plugins=.*/plugins=(git\n extract\n sudo\n autojump\n jsontools\n col
 # Install powerlevel10k and configure it.
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM}"/themes/powerlevel10k
 sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
-
-
+# Move ".zcompdump-*" file to "$ZSH/cache" directory.
 sed -i -e '/source \$ZSH\/oh-my-zsh.sh/i export ZSH_COMPDUMP=\$ZSH\/cache\/.zcompdump-\$HOST' ~/.zshrc
+# Configure the default ZSH configuration for new users.
+sudo cp ~/.zshrc /etc/skel/
+sudo cp ~/.p10k.zsh /etc/skel/
+sudo cp -r ~/.oh-my-zsh /etc/skel/
+sudo chmod -R 755 /etc/skel/
+sudo chown -R root:root /etc/skel/
