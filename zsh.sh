aef8dfab4dc0570e96d873362f1ca9250ad59a9a
aiktb <hey@aiktb.dev>
2024-04-01 23:49:19 +0800

    feat: add more plugins

--- zsh.sh
+++ zsh.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-# Or yum.
 sudo apt install zsh -y
 # Install oh-my-zsh.
-0>/dev/null sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh 0>/dev/null -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 export ZSH_CUSTOM
 # Configure plugins.
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM}"/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git "${ZSH_CUSTOM}"/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search "${ZSH_CUSTOM}"/plugins/zsh-history-substring-search
-sed -i 's/^plugins=.*/plugins=(git\n extract\n sudo\n autojump\n jsontools\n colored-man-pages\n zsh-autosuggestions\n zsh-syntax-highlighting\n zsh-history-substring-search\n)/g' ~/.zshrc
+sudo apt install thefuck autojump -y
+sed -i 's/^plugins=.*/plugins=(git\n extract\n thefuck\n autojump\n jsontools\n colored-man-pages\n zsh-autosuggestions\n zsh-syntax-highlighting\n zsh-history-substring-search\n you-should-use\n nvm\n debian)/g' ~/.zshrc
+# Enable nvm plugin feature to automatically read `.nvmrc` to toggle node version.
+sed -i '1s/^/zstyle ':omz:plugins:nvm' autoload yes\n/' ~/.zshrc
 # Install powerlevel10k and configure it.
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM}"/themes/powerlevel10k
 sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
 # Move ".zcompdump-*" file to "$ZSH/cache" directory.
-sed -i -e "/source \$ZSH\/oh-my-zsh.sh/i export ZSH_COMPDUMP=\$ZSH\/cache\/.zcompdump-\$HOST" ~/.zshrc
+sed -i -e '/source \$ZSH\/oh-my-zsh.sh/i export ZSH_COMPDUMP=\$ZSH\/cache\/.zcompdump-\$HOST' ~/.zshrc
 # Configure the default ZSH configuration for new users.
 sudo cp ~/.zshrc /etc/skel/
 sudo cp ~/.p10k.zsh /etc/skel/
